dc3f317bce41f505d214048928675c05cc792e29
aimon-tnp <tkrupistrai@gmail.com>
2026-02-08 17:31:42 +0700

    complete initialization and schema
    successfully connected and pushed to supabase

---
+++ utils/password.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcryptjs');
+
+const SALT_ROUNDS = 10;
+
+async function hashPassword(plainPassword) {
+  const salt = await bcrypt.genSalt(SALT_ROUNDS);
+  const hash = await bcrypt.hash(plainPassword, salt);
+  return hash;
+}
+
+async function verifyPassword(plainPassword, hash) {
+  if (!hash) return false;
+  return bcrypt.compare(plainPassword, hash);
+}
+
+module.exports = { hashPassword, verifyPassword };
